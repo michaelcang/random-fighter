1e23dab23a8ffc79f9337b67cd42ca861e29f769
NeilTanado <neiltanado@yahoo.co.id>
2018-06-28 20:20:04 +0700

    karya neil

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <Gameplay/>
   </div>
 </template>
+
+<script>
+  import Gameplay from '@/components/Gameplay.vue'
+
+  export default{
+    components : {
+      Gameplay
+    }
+  }
+</script>
