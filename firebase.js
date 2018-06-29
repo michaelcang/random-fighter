64f83fbe3df387a63e4a1b6ef66aaf014054101a
David Joshua <davidjosh_87@yahoo.com>
2018-06-29 15:59:02 +0700

    edit final

--- firebase.js
+++ firebase.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
@@ -11,23 +10,3 @@ const firebaseApp = firebase.initializeApp({
 })
 
 export const db = firebaseApp.database()
-=======
-import Firebase from 'firebase'
-
-const firebaseApp = Firebase.initializeApp({
-  // Populate your firebase configuration data here.
-  apiKey: "AIzaSyBv6kpBoe10a592j2LvlZ0RggHLVojPi-U",
-  authDomain: "randomfighter-12.firebaseapp.com",
-  databaseURL: "https://randomfighter-12.firebaseio.com",
-  projectId: "randomfighter-12",
-  storageBucket: "randomfighter-12.appspot.com",
-  messagingSenderId: "511871249695"
-})
-
-  // Initialize Firebase
-
-
-// Export the database for components to use.
-// If you want to get fancy, use mixins or provide / inject to avoid redundant imports.
-export const db = firebaseApp.database()
->>>>>>> 9a4a92c87139bedfca47affa29e0e5f3a8d2180a
